9e0c8879661a714d8968eaf2fd5383c1dc201043
kennisjoseph <kennisjoseph5@gmail.com>
2023-02-12 23:18:24 +0530

    layout done!!!!

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
     <>
     {/* navbar */}
         <div className='grid max-[730px]:grid-cols-1 max-[1600px]:grid-cols-5 max-[1200px]:grid-cols-4 max-[970px]:grid-cols-3 md:mt-5 pt-9'>
-            <Card imgsrc="/images/luffy.jpg" sound="/images/sound.png"/>
-            <Card imgsrc="/images/brook.jpg" />
-            <Card imgsrc="/images/zoro.jpg" />
-            <Card imgsrc="/images/franky.jpg"/>
-            <Card imgsrc="/images/nami.jpg"/>
-            <Card imgsrc="/images/sanji.jpg"/>
+            <Card imgsrc="/images/luffy.jpg" sound="/images/voice.png"/>
+            <Card imgsrc="/images/brook.jpg" sound="/images/voice.png"/>
+            <Card imgsrc="/images/zoro.jpg" sound="/images/voice.png"/>
+            <Card imgsrc="/images/franky.jpg" sound="/images/voice.png"/>
+            <Card imgsrc="/images/nami.jpg" sound="/images/voice.png"/>
+            <Card imgsrc="/images/sanji.jpg" sound="/images/voice.png"/>
         </div>
         <div className='flex justify-center mt-6'>
             <Button />
